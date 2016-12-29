941cd6027ff9b6650f5d1b43b7848603411c9ee3
nickshevr <nickshevr@google.com>
2016-12-29 19:21:53 +0300

    Update main activity with asyncTask

--- app/src/main/java/com/example/nickshevr/tmps_rss_lab5/MainActivity.java
+++ app/src/main/java/com/example/nickshevr/tmps_rss_lab5/MainActivity.java
@@ -8,8 +8,15 @@ import android.widget.ListView;
 import  com.example.nickshevr.tmps_rss_lab5.RssItem;
 import  com.example.nickshevr.tmps_rss_lab5.ListListener;
 import  com.example.nickshevr.tmps_rss_lab5.RssReader;
+import java.util.List;
+import android.os.AsyncTask;
 
 public class MainActivity extends Activity {
+    /**
+     * This method creates main application view
+     */
+    private MainActivity local;
+
     /**
      * This method creates main application view
      */
@@ -18,19 +25,53 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         // Set view
         setContentView(R.layout.activity_main);
-        try {
-            // Create RSS reader
-            RssReader rssReader = new RssReader("http://news.yandex.ru/politics.rss");
+
+        // Set reference to this activity
+        local = this;
+
+        GetRSSDataTask task = new GetRSSDataTask();
+
+        // Start download RSS task
+        task.execute("http://news.yandex.ru/politics.rss");
+
+        // Debug the thread name
+        Log.d("Thread", Thread.currentThread().getName());
+    }
+
+    private class GetRSSDataTask extends AsyncTask<String, Void, List<RssItem> > {
+        @Override
+        protected List<RssItem> doInBackground(String... urls) {
+
+            // Debug the task thread name
+            Log.d("Thread", Thread.currentThread().getName());
+
+            try {
+                // Create RSS reader
+                RssReader rssReader = new RssReader(urls[0]);
+
+                // Parse RSS, get items
+                return rssReader.getItems();
+
+            } catch (Exception e) {
+                Log.e("Thread", e.getMessage());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<RssItem> result) {
+
             // Get a ListView from main view
             ListView itcItems = (ListView) findViewById(R.id.listMainView);
+
             // Create a list adapter
-            ArrayAdapter adapter = new ArrayAdapter<RssItem>(this,android.R.layout.simple_list_item_1, rssReader.getItems());
+            ArrayAdapter<RssItem> adapter = new ArrayAdapter<RssItem>(local,android.R.layout.simple_list_item_1, result);
             // Set list adapter for the ListView
             itcItems.setAdapter(adapter);
+
             // Set list view item click listener
-            itcItems.setOnItemClickListener(new ListListener(rssReader.getItems(), this));
-        } catch (Exception e) {
-            Log.e("ITCRssReader", e.getMessage());
+            itcItems.setOnItemClickListener(new ListListener(result, local));
         }
     }
 }
